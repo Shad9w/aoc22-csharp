0003d9750c954dd207a5b875dc450aee0fe1e772
Shad0w <ioni.xyz@gmail.com>
2022-12-06 23:13:08 +0200

    Create day6.cs

---
+++ day6.cs
@@ -0,0 +1,63 @@
+// Read file
+string[] lines = File.ReadAllLines("../../../day6.txt");
+
+bool moving = false;
+
+string[][] crates= new string[lines.Length][];
+
+int total_lines = 9;
+
+List<string>[] myList = new List<string>[total_lines];
+for (int i = 0; i < total_lines; i++)
+{
+    myList[i] = new List<string>();
+}
+
+
+
+for (int i = 0; i < lines.Length; i++)
+{
+    if(!moving)
+    {
+        crates[i] = new string[lines[i].Length];
+        for(int idx = 0; idx < lines[i].Length; idx++)
+        {
+            if((idx - 1) % 4 == 0)
+            {
+                if (lines[i][idx].ToString() != " ")
+                {
+                    myList[(idx - 1)/4].Add(lines[i][idx].ToString());
+                }
+
+            }
+        }
+
+    }
+
+    // empty line
+    if (lines[i] == "")
+    {
+        moving = true;
+        continue;
+    }
+
+    if (moving)
+    {
+        string[] moves = lines[i].Split(" ");
+
+        // 1 - how many, 3 - from, 5 - to
+        for(int m = 0; m < int.Parse(moves[1]); m++)
+        {
+            myList[int.Parse(moves[5]) - 1].Insert(0, myList[int.Parse(moves[3]) - 1].First());
+            myList[int.Parse(moves[3]) - 1].RemoveAt(0);
+        }
+
+
+    }
+}
+
+for(int i = 0; i < total_lines; i++)
+{
+    Console.Write(myList[i].First());
+}
+
